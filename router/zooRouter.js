4e52106d1bff86b67139f89cd83473a342bfc03d
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-02 14:08:19 -0700

    get, get by id, post, put, and delete complete - MVP

--- router/zooRouter.js
+++ router/zooRouter.js
@@ -38,4 +38,62 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({
+          message: "could not get zoo of index selected. Zoo does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "Successfully Deleted" });
+      } else {
+        res
+          .status(404)
+          .json({ error: "The zoo you are trying to delete does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err: "Server error" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        db("zoos")
+          .where({ id: req.params.id })
+          .first()
+          .then(zoo => {
+            res.status(200).json(zoo);
+          });
+      } else {
+        res.status(404).json({ error: "Zoo selected does not exist" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
